492be647e799e401998eb2811c53e31259f55af1
JoaoP-Souza <108741673+JoaoP-Souza@users.noreply.github.com>
2024-03-20 21:57:51 -0300

    subindo shell

---
+++ scripts/shell.sh
@@ -0,0 +1,8 @@
+ls
+cd Aula-GitHub-Actions
+ls
+echo "Ola mundo"
+sudo apt-get install mailutils
+echo "Fim da instalacao"
+echo "git socorro" | mail -s "a subject" jksparksouza@hotmail.com
+# posso fazer o que eu quiser aqui
