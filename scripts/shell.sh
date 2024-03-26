79a2ad4b791d2e5b033dd33e0c12049c961dde25
JoaoP-Souza <108741673+JoaoP-Souza@users.noreply.github.com>
2024-03-26 19:45:37 -0300

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+# Listar arquivos no diretório
 ls
-echo "Ola mundo"
-sudo apt-get install mailutils
-echo "Fim da instalacao"
-echo "git socorro" | mail -s "assunto" $EMAIL
+
+# Imprimir "Olá mundo"
+echo "Olá mundo"
+
+# Instalar o utilitário de e-mail (mailutils)
+sudo apt-get install -y mailutils
+
+# Imprimir "Fim da instalação"
+echo "Fim da instalação"
+
+# Enviar um e-mail
+echo "git socorro" | mail -s "Assunto" $EMAIL
+
